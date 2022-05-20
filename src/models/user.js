2f91f353a5a8db4a5e57c17df0b550c609b8b243
Gabriel Peroni <gabrielcperoni@gmail.com>
2022-05-20 11:49:52 -0300

    models_user

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const { v4: uuidv4 } = require('uuid');
+const connection = require("../database/connection");
+
+module.exports = {
+    async create(usuario) {
+        const user_id = uuidv4();
+        usuario.user_id = user_id;
+
+        const result = await connection("usuario").insert(usuario);
+            return result;
+    },
+    
+    async getById( user_id ){
+        const result = await connection("usuario").where({user_id}).select("*").first();
+        return result;
+    },
+
+    async updateById(user_id, usuario) {
+        const result = await connection("usuario").where(user_id).update(usuario);
+        return result;
+    },
+
+    async deleteById(user_id) {
+        const result = await connection("usuario").where(user_id).delete();
+        return result;
+    },
+};
