49125f5f8b8d96211f2332cabe2a2be5e996b9f7
Gabriel Peroni <gabrielcperoni@gmail.com>
2022-05-23 20:39:00 -0300

    Validator User

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const routes = express.Router();
 
 const UserController = require("./controllers/UserController");
+const UserValidator = require ("./validators/UserValidator");
 
-routes.post('/user', UserController.create);
-routes.get('/user/:user_id', UserController.getById);
-routes.put('/user/:user_id', UserController.update);
-routes.delete('/user/:user_id', UserController.delete);
+routes.post('/user', UserValidator.create, UserController.create);
+routes.get('/user/:user_id', UserValidator.getById, UserController.getById);
+routes.put('/user/:user_id', UserValidator.update, UserController.update);
+routes.delete('/user/:user_id', UserValidator.delete, UserController.delete);
 
 module.exports = routes;
