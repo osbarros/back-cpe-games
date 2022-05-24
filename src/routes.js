99ff696b2237be480f38f00ade31763c00e9cb1b
osbarros <ceobarros@outlook.com>
2022-05-24 14:37:17 -0300

    Correção do arquivo routes

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const JogoValidator = require("./validators/JogoValidator");
 // Jogo
 routes.post("/jogo", JogoValidator.create, JogoController.create);
 routes.get("/jogo/:jogo_id", JogoValidator.getById, JogoController.getById);
-routes.put("/jogo/:jogo_id", JogoValidator.update, JogoController.update);
-routes.delete("/jogo/:jogo_id", JogoValidator.delete, JogoController.delete);
+routes.put("/jogo/:jogo_id", JogoValidator.updateById, JogoController.update);
+routes.delete("/jogo/:jogo_id", JogoValidator.deleteById, JogoController.delete);
 
 module.exports = routes;
