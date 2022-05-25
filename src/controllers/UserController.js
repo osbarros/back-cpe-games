0e33dd5ab9d751538b8adc24bbbc7984c581e703
osbarros <ceobarros@outlook.com>
2022-05-25 11:32:05 -0300

    Acréscimo da entidade jogos à main

---
+++ src/controllers/UserController.js
@@ -0,0 +1,69 @@
+const UserModel = require("../models/UserModel");
+const firebase = require("../utils/Firebase");
+
+module.exports = {
+  async create(request, response) {
+    try{
+      const newUser = request.body;
+      const uid = await firebase.createNewUser(newUser.email, newUser.senha);
+
+      delete newUser.senha;
+      newUser.firebase_id = uid;
+      const result = await UserModel.create(newUser);
+
+      return response.status(200).json(result);
+    } catch (error) {
+      console.warn("User creation failed:", error);
+      return response.status(500).json({
+        notification: "Internal server error while trying to create User",
+      });
+    }
+  },
+  async getById(request, response) {
+    try {
+      const { user_id } = request.params;
+      const result = await UserModel.getById(user_id);
+
+      return response.status(200).json(result);
+    } catch (error) {
+      console.warn("User getById failed: ", error);
+      return response.status(500).json({
+        notification: "Internal server error while trying to get User",
+      });
+      
+    }
+  },
+  async update(request, response) {
+    try {
+      const { user_id } = request.params;
+      const newUser = request.body;
+      const result = await UserModel.updateById(user_id, newUser);
+
+      if (result === 0)
+        return response.status(400).json({ notification: "user_id not found"});
+
+      return response.status(200).json({notification: "User updated successfully."});
+    } catch (error) {
+      console.warn("User update failed: ", error);
+      return response.status(500).json({
+        notification: "Internal server error while trying to update User",
+      });
+    }
+  },
+  async delete(request, response) {
+    try {
+      const { user_id } = request.params;
+      const result = await UserModel.deleteById(user_id);
+
+      if (result === 0)
+        return response.status(400).json({ notification: "user_id not found"});
+
+      return response.status(200).json({notification: "User deleted successfully."});
+    } catch (error) {
+      console.warn("User delete failed: ", error);
+      return response.status(500).json({
+        notification: "Internal server error while trying to delete User",
+      });
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,19 @@ const routes = express.Router();
 const JogoController = require("./controllers/JogoController");
 const JogoValidator = require("./validators/JogoValidator");
 
+const UserController = require("./controllers/UserController");
+const UserValidator = require ("./validators/UserValidator");
+
 // Jogo
 routes.post("/jogo", JogoValidator.create, JogoController.create);
 routes.get("/jogo/:jogo_id", JogoValidator.getById, JogoController.getById);
 routes.put("/jogo/:jogo_id", JogoValidator.updateById, JogoController.update);
 routes.delete("/jogo/:jogo_id", JogoValidator.deleteById, JogoController.delete);
+
+// User
+routes.post('/user', UserValidator.create, UserController.create);
+routes.get('/user/:user_id', UserValidator.getById, UserController.getById);
+routes.put('/user/:user_id', UserValidator.update, UserController.update);
+routes.delete('/user/:user_id', UserValidator.delete, UserController.delete);
 
 module.exports = routes;
