9d61515d268e0bd334ae7d0920bd5a1470791eb1
samuel rebula <rebuuula@gmail.com>
2022-05-26 02:20:31 -0300

    revisão do firebase

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -14,20 +14,20 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 module.exports = {
-    async createNewUser(email, password){
+    async createNewUser(email, password){ //cria o login do usuário com base no email e na senha
       const result = await firebase
         .auth()
-        .createUserWithEmailAndPassword(email, password);
+        .createUserWithEmailAndPassword(email, password); // cria o user
 
-      return result.user.uid;
+      return result.user.uid; //user id
     },
 
     async login(email, password){
       
-        const result = await firebase
+        const result = await firebase // busca o login no firebase
           .auth()
-          .signInWithEmailAndPassword(email, password);
+          .signInWithEmailAndPassword(email, password);  //login com o usuario ja cadastrado no sistema
 
-        return result.user.id;
+        return result.user.id; // user id
     },
 };
